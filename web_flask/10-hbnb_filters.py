128d20990405ba8cc510a0fd21b4f7d5c939ffd1
Babatunde Adeoti <babaolu98@gmail.com>
2024-03-31 15:20:54 +0100

    Modifying static filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,8 +3,9 @@
 from flask import Flask, render_template
 from markupsafe import escape
 from models import storage
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
+from models.state import State
 
 app = Flask(__name__)
 
@@ -15,18 +16,13 @@ def tear_down(exception=None):
     storage.close()
 
 
-@app.route("/states", strict_slashes=False)
+@app.route("/hbnb_filters", strict_slashes=False)
 def list_states():
     """ Display States and their assoiciate cities """
     state_list = [v for k, v in storage.all("State").items()]
-    return render_template('7-states_list.html', states=state_list)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def cities_by_state(id):
-    """ Display the assoiciates cities of the given state """
-    fstate = storage.all().get("State." + id)
-    return render_template('9-states.html', state=fstate)
+    amenity_list = [v for k, v in storage.all("Amenity").items()]
+    return render_template('10-hbnb_filters.html', states=state_list,
+                           amenities=amenity_list)
 
 
 if __name__ == "__main__":
